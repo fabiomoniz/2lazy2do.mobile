ab974b6c109eb21a3ce28f0fb7dd823d532ac923
ZeXVex <niclaiwork@live.dk>
2019-05-01 11:05:18 +0200

    show menu button in toolbar

--- app/src/main/java/com/example/a2lazy2do/MainActivity.java
+++ app/src/main/java/com/example/a2lazy2do/MainActivity.java
@@ -1,9 +1,14 @@
 package com.example.a2lazy2do;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ListView;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 import com.example.a2lazy2do.BE.Task;
 
@@ -11,9 +16,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar mToolbar;
     ListView mListView;
-
+    Toolbar mToolbar;
     ArrayList<Task> tasks;
 
     @Override
@@ -21,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mListView = (ListView) findViewById(R.id.listview);
 
         tasks = new ArrayList<>();
@@ -30,5 +34,24 @@ public class MainActivity extends AppCompatActivity {
 
         MyAdapter myAdapter = new MyAdapter(MainActivity.this , tasks);
         mListView.setAdapter(myAdapter);
+        mToolbar.inflateMenu(R.menu.main_menu);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem menu) {
+        switch (menu.getItemId()) {
+            case R.id.action_newList:
+                return true;
+                default:
+                    return super.onOptionsItemSelected(menu);
+        }
+    }
+
 }
+
