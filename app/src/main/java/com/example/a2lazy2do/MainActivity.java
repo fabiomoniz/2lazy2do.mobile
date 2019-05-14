fc0a9b861fc22deaea5e4a4a08168d7311b1f1ec
fabiomoniz <fabio_moniz_1@hotmail.com>
2019-05-14 03:33:19 +0200

    login done

--- app/src/main/java/com/example/a2lazy2do/MainActivity.java
+++ app/src/main/java/com/example/a2lazy2do/MainActivity.java
@@ -1,29 +1,56 @@
 package com.example.a2lazy2do;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ListView;
-import android.support.v7.widget.Toolbar;
 import com.example.a2lazy2do.BE.Task;
-import java.util.ArrayList;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.firebase.auth.FirebaseAuth;
 
-import static com.example.a2lazy2do.R.id.action_newList;
-import static com.example.a2lazy2do.R.id.action_signIn;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static com.example.a2lazy2do.R.id.action_signOut;
 
 public class MainActivity extends AppCompatActivity {
 
-    Toolbar mToolbar;
-    ListView mListView;
 
+    ListView mListView;
     ArrayList<Task> tasks;
+    FirebaseAuth auth;
+    private static final int RC_SIGN_IN = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        auth = FirebaseAuth.getInstance();
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+                new AuthUI.IdpConfig.EmailBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build());
+
+        if(auth.getCurrentUser() != null){
+            //user already signed in
+            Log.d("AUTH", auth.getCurrentUser().getEmail());
+        }else{
+            startActivityForResult(
+                    AuthUI.getInstance()
+                            .createSignInIntentBuilder()
+                            .setAvailableProviders(providers)
+                            .build(),
+                    RC_SIGN_IN);
+        }
+
+
 
         mListView = (ListView) findViewById(R.id.listview);
 
@@ -39,6 +66,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RC_SIGN_IN){
+            if(requestCode == RESULT_OK){
+                Log.d("AUTH", auth.getCurrentUser().getEmail());
+            }else {
+                //user not authenticated
+                Log.d("AUTH", "NOT AUTHENTICATED");
+            }
+        }
+    }
+
+    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.main_menu, menu);
         return true;
@@ -47,17 +87,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-            case action_signIn:
-                clickSignIn();
+            case action_signOut:
+                clickSignOut();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    public void clickSignIn(){
-        Intent x = new Intent(this, SignInActivity.class);
-        startActivity(x);
+    public void clickSignOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull com.google.android.gms.tasks.Task<Void> task) {
+                        Log.d("AUTH", "USER LOGGED OUT");
+                        finish();
+                    }
+                });
     }
 
 }
